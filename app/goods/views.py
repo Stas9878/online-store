6c18f1e443885916d58a19c90aac8835957a6666
Popov Stas <boss.staspopov@mail.ru>
2024-02-11 14:10:44 +0300

    Добавил модель category

--- app/goods/views.py
+++ app/goods/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
+from templates.other_files.goods_list import goods
 
 def catalog(request):
-    return render(request, 'goods/catalog.html')
+    context = {
+        'goods': goods
+    }
+    return render(request, 'goods/catalog.html', context=context)
 
 def product(request):
     return render(request, 'goods/product.html')
