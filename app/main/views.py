5a8dc5548dd9894dc1f0f9a8f16281afbfb05e0b
Popov Stas <boss.staspopov@mail.ru>
2024-02-11 13:06:45 +0300

    Добавил станицы index и about в приложение main

--- app/main/views.py
+++ app/main/views.py
@@ -2,10 +2,15 @@ from django.shortcuts import render
 
 def index(request):
     context = {
-        'title': 'Главная',
-        'content': 'Something'
+        'title': 'O.LIB - Главная',
+        'content': 'Онлайн магазин книг'
     }
     return render(request, 'index.html', context=context)
 
 def about(request):
-    return render(request, 'about.html')
+    context = {
+        'title': 'O.Lib - О нас',
+        'content': 'О нас',
+        'text_on_page': 'Крутой магазин книг'
+    }
+    return render(request, 'about.html', context=context)
