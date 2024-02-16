f0cb428ba7e26c41d9ed12753e577599b8b72dcc
Popov Stas <boss.staspopov@mail.ru>
2024-02-16 16:33:02 +0300

    Создал таблицу Products и обновил админ панель

--- app/goods/models.py
+++ app/goods/models.py
@@ -1,8 +1,25 @@
 from django.db import models
 
 class Categories(models.Model):
-    name = models.CharField(max_length=150, unique=True)
-    slug = models.SlugField(max_length=200, unique=True, blank=True, null=True)
+    name = models.CharField(max_length=150, unique=True, verbose_name='Название')
+    slug = models.SlugField(max_length=200, unique=True, blank=True, null=True, verbose_name='URL')
     
     class Meta:
         db_table = 'category'
+        verbose_name = 'Категорию'
+        verbose_name_plural = 'Категории'
+
+class Products(models.Model):
+    name = models.CharField(max_length=150, unique=True, verbose_name='Название')
+    slug = models.SlugField(max_length=200, unique=True, blank=True, null=True, verbose_name='URL')
+    description = models.TextField(blank=True, null=True, verbose_name='Описание')
+    image = models.ImageField(upload_to='goods_images', blank=True, null=True, verbose_name='Фото')
+    price = models.IntegerField(default=0, verbose_name='Цена')
+    discount = models.IntegerField(default=0, verbose_name='Скидка в %')
+    quantity = models.PositiveIntegerField(default=0, verbose_name='Количество')
+    category = models.ForeignKey(Categories, on_delete=models.CASCADE, verbose_name='Категория')
+
+    class Meta:
+        db_table = 'product'
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товары'
