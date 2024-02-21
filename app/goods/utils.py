660c3a115800e5bc4c1c4ab92b454d6e79945d75
Popov Stas <boss.staspopov@mail.ru>
2024-02-21 16:27:17 +0300

    Добавил полнотекстовый поиск

--- app/goods/utils.py
+++ app/goods/utils.py
@@ -1,14 +1,31 @@
-from django.db.models import Q
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank, SearchHeadline
 from .models import Products
+
+def annotate_result_search(result_search, query):
+    result_search = result_search.annotate(
+        headline=SearchHeadline(
+        "name",
+        query,
+        start_sel="<span style='background-color: yellow;'>",
+        stop_sel="</span>",
+        )
+    )
+
+    result_search = result_search.annotate(
+        bodyline=SearchHeadline(
+        "description",
+        query,
+        start_sel="<span style='background-color: yellow;'>",
+        stop_sel="</span>",)
+    )
+
+    return result_search
 
 def q_search(query):
     if query.isdigit() and len(query) <= 5:
         return Products.objects.filter(id=int(query))
     
-    keywords = [word for word in query.split() if len(word) > 2]
-    q_objects = Q()
-    for token in keywords:
-        q_objects |= Q(description__icontains=token)
-        q_objects |= Q(name__icontains=token)
-
-    return Products.objects.filter(q_objects)
+    vector = SearchVector('name', 'description')
+    query = SearchQuery(query)
+    result_search = Products.objects.annotate(rank=SearchRank(vector, query)).filter(rank__gt=0).order_by('-rank')
+    return annotate_result_search(result_search, query)
