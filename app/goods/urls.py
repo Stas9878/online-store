d14eca6903306175890c63d4948f8dbd517da888
Popov Stas <boss.staspopov@mail.ru>
2024-02-21 14:53:16 +0300

    Реализовал поиск по сайту

--- app/goods/urls.py
+++ app/goods/urls.py
@@ -4,6 +4,7 @@ from .views import *
 app_name = 'catalog'
 
 urlpatterns = [
+    path('search/', catalog, name='search'),
     path('<slug:category_slug>/', catalog, name='catalog'),
     path('product/<slug:slug>/', product, name='product'),
 ]
