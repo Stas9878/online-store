a836b66cd96831b5a3c56baceb8d0f19e375c312
Popov Stas <boss.staspopov@mail.ru>
2024-02-24 12:01:27 +0300

    Добавление возможности удаления товара из корзины

--- app/carts/urls.py
+++ app/carts/urls.py
@@ -4,8 +4,8 @@ from .views import *
 app_name = 'carts'
 
 urlpatterns = [
-    path('cart-add/<slug:product_slug>/', cart_add, name='cart_add'),
-    path('cart-change/<slug:product_slug>/', cart_change, name='cart_change'),
-    path('cart-remove/<slug:product_slug>/', cart_remove, name='cart_remove'),
+    path('cart_add/<slug:product_slug>/', cart_add, name='cart_add'),
+    path('cart_change/<slug:product_slug>/', cart_change, name='cart_change'),
+    path('cart_remove/<int:cart_id>/', cart_remove, name='cart_remove'),
     
 ]
